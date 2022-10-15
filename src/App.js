0c643454a5d2699dbdf811c8fad3cdc57bbecb6b
Robin Hsu <robin.h@mydailylive.com>
2022-10-16 02:15:33 +0800

    weather web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { AirFlow, Celsius, Container, CurrentWeather, Description, Location, Rain, Refresh, Temperature, WeatherCard } from './customize';
+import React from 'react';
+import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg'
+import { ReactComponent as AirFlowIcon } from './images/airFlow.svg'
+import { ReactComponent as RainIcon } from './images/rain.svg'
+import { ReactComponent as RefreshIcon } from './images/refresh.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <WeatherCard>
+        <Location theme='dark'>Taipei City</Location>
+        <Description>Cloudy</Description>
+        <CurrentWeather>
+          <Temperature>
+            23<Celsius>°C</Celsius>
+          </Temperature>
+          <DayCloudyIcon />
+        </CurrentWeather>
+        <AirFlow>23 m/h</AirFlow>
+        <Rain>48%</Rain>
+        <Refresh> Last Time: Am 12:03</Refresh>
+      </WeatherCard>
+    </Container>
   );
 }
 
---
+++ src/customize.js
@@ -0,0 +1,76 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  background-color: #ededed;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999999;
+  background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
+
+export const Location = styled.div`
+  font-size: 28px;
+  color: ${props => props.theme === 'dark' ? '#dadada' : '#212121'};
+  margin-bottom: 20px;
+`;
+
+export const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`;
+
+export const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+export const Temperature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+export const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+export const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+`;
+
+export const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+`;
+
+export const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: #828282;
+`;
